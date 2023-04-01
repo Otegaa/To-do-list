c375e31fecedb1f843b7d97bf8d14c1e1be2257a
otegaa <ekiugbotega@gmail.com>
2023-04-01 17:55:14 +0200

    Implement function for checkbox

--- modules/addRemove.js
+++ modules/addRemove.js
@@ -2,6 +2,7 @@ const list = document.querySelector('.to-do-list');
 const addText = document.querySelector('.to-do-add-text');
 const form = document.querySelector('.form');
 const enterBtn = document.querySelector('.enter-btn');
+const clearBtn = document.querySelector('.to-do-clear');
 
 const wholeList = function () {
   let getStorage = JSON.parse(localStorage.getItem('list')) || [];
@@ -15,12 +16,18 @@ const wholeList = function () {
       list.innerHTML += `
          <li class="list-task">
             <div class="to-do-list-task">
-              <input type="checkbox" class="to-do-input" id= ${item.index}/>
-              <p class="to-do-text" id= ${item.index}>${item.desc}</p>
+              <input type="checkbox" class="to-do-input" id= ${
+  item.index - 1
+} />
+              <p class="to-do-text" id= ${item.index - 1} >${item.desc}</p>
             </div>
             <div class="to-do-btn-del">
-              <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${item.index}  ></i>
-              <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id= ${item.index}></i>
+              <i class="fas fa-ellipsis-v fa-2x remove-list" id= ${
+  item.index
+}  ></i>
+              <i class="fa-solid fa-trash-can fa-2x hidden add-trash" id= ${
+  item.index
+}></i>
             </div>
           </li>`;
     });
@@ -81,9 +88,11 @@ const wholeList = function () {
     editList(target);
   });
 
+  // Function to implement when focused out on list
+
   list.addEventListener('focusout', (e) => {
     const target = e.target.closest('.to-do-text');
-    const targetID = Number(target.id - 1);
+    const targetID = Number(e.target.id);
     if (!target) return;
     if (target) {
       getStorage[targetID].desc = target.textContent;
@@ -92,6 +101,49 @@ const wholeList = function () {
       window.location.reload();
     }
   });
+
+  // Function to check box
+  list.addEventListener('change', (e) => {
+    const target = e.target.closest('.to-do-input');
+    const text = e.target.nextElementSibling;
+    const targetID = Number(e.target.id);
+
+    if (!target) return;
+    if (target.checked) {
+      text.style.textDecoration = 'line-through';
+      getStorage[targetID].completed = true;
+      setStorage();
+    } else {
+      text.style.textDecoration = 'none';
+      getStorage[targetID].completed = false;
+      setStorage();
+    }
+  });
+
+  const domLoaded = () => {
+    const text = document.querySelectorAll('.to-do-text');
+    const checkbox = [...document.querySelectorAll('.to-do-input')];
+    getStorage.forEach((item, i) => {
+      if (item.completed === true) {
+        checkbox[i].checked = true;
+        text[i].style.textDecoration = 'line-through';
+      }
+    });
+  };
+
+  window.addEventListener('DOMContentLoaded', domLoaded);
+
+  // Function to delete all selected buttons.
+  const delBtn = () => {
+    getStorage = getStorage.filter((item) => item.completed === false);
+    getStorage.forEach((item, i) => {
+      item.index = i + 1;
+    });
+    setStorage();
+    displayList();
+  };
+
+  clearBtn.addEventListener('click', delBtn);
 
   displayList();
 };
